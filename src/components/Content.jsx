1d1a6ac44ea073497676779c6b1c6e028ee2d6c6
kaanmetep <kaanpmete@gmail.com>
2024-04-07 02:56:38 +0300

    design changes for mobile

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,7 @@ import Memories from "./Memories";
 function Content() {
   return (
     <div>
-      <h2 className="mb-12 text-2xl text-center">
+      <h2 className="lg:mb-12 mb-8 lg:text-2xl  text-lg text-center">
         see the memories from all over the world
       </h2>
       <Memories />
--- src/components/HeaderBottom.jsx
+++ src/components/HeaderBottom.jsx
@@ -3,16 +3,16 @@ import User from "./User";
 function HeaderBottom({ handleSignInClicked, handleSignupClicked }) {
   const userLoggedIn = false;
   return (
-    <div className="border-solid border-b-4 border-t-4 pb-6 border-red-100 my-12">
+    <div className="border-solid border-b-4 border-t-4 pb-6 border-red-100 mt-12 lg:mb-48 mb-32">
       {userLoggedIn === false ? (
         <div className="mt-4 flex flex-col gap-3">
-          <div className="flex gap-3 items-center justify-center">
-            <h2 className="font-semibold text-lg">
+          <div className="flex lg:gap-4 gap-2 items-center justify-center">
+            <h2 className="font-semibold lg:text-lg text-sm text-center">
               Sign up now to share your amazing memories with everyone else!
             </h2>
             <Button
               onClick={handleSignupClicked}
-              className="font-bold px-8 py-2"
+              className="font-bold  py-2 lg:text-base text-xs px-8 shrink-0 "
             >
               Sign up
             </Button>
